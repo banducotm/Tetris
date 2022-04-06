ef8ff05191d8ff650dcbced53d53e765ca68d56f
João Henrique <100325798+banducotm@users.noreply.github.com>
2022-04-05 23:14:55 -0300

    Tetris
    codigo final

--- Tetris/App.xaml.cs
+++ Tetris/App.xaml.cs
@@ -3,10 +3,9 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
 
-namespace WpfApp1
+namespace Tetris
 {
     /// <summary>
     /// Interaction logic for App.xaml
---
+++ Tetris/Login.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Tetris
+{
+    /// <summary>
+    /// Lógica interna para Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void loginbutton_Click(object sender, RoutedEventArgs e)
+        {
+            string usuario = loginbox.Text;
+            string senha = passbox.Password.ToString();
+
+            if (usuario == "joaozinhogamer" && senha == "123")
+            {
+                MainWindow janela = new MainWindow();
+                janela.ShowDialog();
+            }
+        }
+    }
+}
